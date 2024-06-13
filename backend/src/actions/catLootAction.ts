80aa25bd3bb6a48fd07100389a2d6b14381ff986
Justin Feng <realfengjia@foxmail.com>
2024-06-13 14:42:23 +0800

    chore: 🎸 backend clean up

--- backend/src/actions/catLootAction.ts
+++ backend/src/actions/catLootAction.ts
@@ -1,17 +1,13 @@
-import { ethers } from 'ethers';
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-import { serverWallet } from '../constant';
-import { createIdAttest } from '../services/externalApi';
-import { getEligibilityStatus } from '../services/launchpadApi';
+import { z } from 'zod';
 import { Action } from '../_core/type';
-import { verifyWalletPostSchema, VerifyWalletRequest } from './requestSchemas';
 
 export const verifyWallet: Action = {
   path: '/verify-wallet',
   method: 'post',
   options: {
     schema: {
-      body: verifyWalletPostSchema,
+      body: z.any(),
     },
   },
   handler: verifyWalletHandler,
@@ -22,48 +18,5 @@ async function verifyWalletHandler(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const data = request.body as VerifyWalletRequest;
-
-  const receiver = ethers.verifyMessage(data.message, data.signature);
-  if (receiver !== data.receiver) {
-    return reply.status(401).send({ message: 'Invalid signature' });
-  }
-
-  const status = await getEligibilityStatus(receiver);
-
-  if (status.data.claimingStartAt > Date.now()) {
-    return reply.status(400).send({ message: 'Claiming not started yet' });
-  }
-
-  if (
-    !(
-      (['cat', 'catLoot'].includes(status.data.stakedAsset) || status.data.staking) &&
-      status.data.discordJoined &&
-      status.data.twitterFollowed
-    )
-  ) {
-    return reply.status(401).send({ message: 'Not eligible to claim' });
-  }
-
-  try {
-    const id = {
-      idType: 'cat-loot-holder',
-      value: data.receiver,
-    };
-    const result = await createIdAttest(
-      id,
-      'validated',
-      1, //1 hour
-      await serverWallet.signMessage(JSON.stringify(id)),
-      data.receiver
-    );
-
-    return reply.status(201).send({ attestation: result.data.rawData });
-  } catch (e: any) {
-    if (e.response && e.response.data) {
-      return reply.status(500).send(e.response.data);
-    } else {
-      return reply.status(500).send({ message: e.message });
-    }
-  }
+  return reply.status(200).send({ message: 'OK' });
 }
--- backend/src/controllers.ts
+++ backend/src/controllers.ts
@@ -1,35 +1,7 @@
-import {
-  createClaimingOffer,
-  createDelivery,
-  createIdAttestAction,
-  createSellingOffer,
-  getAttestationRawdata,
-} from "./actions/attestationAction";
-import { createOtp } from "./actions/secretAction";
-
-import { fetchNonce, verifyWallet } from "./actions/redbrickAction";
 import { verifyWallet as verifyCatLootWallet } from "./actions/catLootAction";
 import { Controller, SecurityFilterRule } from "./_core/type";
 
 export const controllers: Controller[] = [
-  {
-    prefix: "attestations",
-    actions: [
-      createSellingOffer,
-      createClaimingOffer,
-      createDelivery,
-      createIdAttestAction,
-      getAttestationRawdata,
-    ],
-  },
-  {
-    prefix: "secret",
-    actions: [createOtp],
-  },
-  {
-    prefix: "redbrick",
-    actions: [fetchNonce, verifyWallet],
-  },
   {
     prefix: "cat-loot",
     actions: [verifyCatLootWallet],
@@ -37,6 +9,4 @@ export const controllers: Controller[] = [
 ];
 
 export const securityRules: SecurityFilterRule[] = [
-  { pattern: /^\/attestations/ },
-  { pattern: /^\/redbrick\/verfiy-wallet/ },
 ];
