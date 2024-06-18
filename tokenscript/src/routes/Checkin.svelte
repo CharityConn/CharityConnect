5e0b1f0d8d59a83dce83ac7d372b989a3b2b80d3
Hwee-Boon Yar <hboon@motionobj.com>
2024-06-18 13:44:36 +0800

    feat: 🎸 add TokenScript project as subfolder; with TS for check-in, triggered by user

---
+++ tokenscript/src/routes/Checkin.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import context from "../lib/context";
+	import Loader from "../components/Loader.svelte";
+
+	let token;
+    let walletAddress;
+	let merchantID = "";
+	let successMessage = "";
+	let loading = true;
+
+	context.data.subscribe(async (value) => {
+		if (!value.token)
+			return;
+		token = value.token;
+        walletAddress = value.token.ownerAddress;
+		// You can load other data before hiding the loader
+		loading = false;
+	});
+
+	async function checkin(){
+        console.log("Checkin...");
+
+		loading = true;
+
+		try {
+            const method = 'POST';
+            const headers: any = {
+                "Content-type": "application/json",
+                "Accept": "application/json",
+            };
+            const userWallet = walletAddress;
+            //TODO change URL. How to read from a config?
+            const res = await fetch(`http://localhost:3206/user/${userWallet}/checkin`, {
+                method,
+                headers,
+                body: JSON.stringify({ merchantID })
+            });
+            let data: any;
+            data = await res.json();
+            console.log(`Status code: ${res.status}`)
+			successMessage = 'Done checkin';
+		} catch (e){
+			successMessage = 'Failed to checkin';
+			console.error(e);
+		}
+
+		loading = false;
+	}
+
+</script>
+
+<div>
+	{#if token}
+		<h3>Checkin</h3>
+
+		<div id="message-title">
+            Merchant ID:
+        </div>
+		<textarea class="message-input" bind:value={merchantID} placeholder="e.g 1234" disabled={loading}></textarea>
+
+        {#if !successMessage.length}
+            <div class="link-button" style="position: relative; margin: 0 auto; max-width: 200px;">
+                <button type="button" on:click={() => checkin()} disabled={loading || !walletAddress.length || !merchantID.length || !!successMessage.length}>
+                Checkin
+                </button>
+            </div>
+        {:else}
+            <p>{successMessage}</p>
+        {/if}
+
+	{/if}
+	<Loader show={loading}/>
+</div>
