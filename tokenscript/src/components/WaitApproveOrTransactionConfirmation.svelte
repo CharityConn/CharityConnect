b44c1acb980308a7c3425f233db13de9e85c166f
Hwee-Boon Yar <hboon@motionobj.com>
2024-09-05 14:16:45 +0800

    feat: 🎸 style Ongoing transaction (pending signature/post transaction)

--- tokenscript/src/components/WaitApproveOrTransactionConfirmation.svelte
+++ tokenscript/src/components/WaitApproveOrTransactionConfirmation.svelte
@@ -3,6 +3,9 @@
 </script>
 
 {#if show}
-	<h3>Ongoing transaction</h3>
-	<p>You need to approve this transaction in your wallet</p>
+	<div class="flex flex-col items-center">
+		<img src="https://d31vrfdo6b6g17.cloudfront.net/assets/images/loading-icon.gif" alt="Icon" class="w-16 h-16 mt-12">
+		<h3 class="text-xl font-semibold mt-4">Ongoing transaction</h3>
+		<p class="text text-gray-600 mt-3 mx-6 text-center">You need to approve this transaction in your wallet</p>
+	</div>
 {/if}
