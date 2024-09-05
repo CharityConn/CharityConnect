c96f2bc0c83a42476de51201f611c931e4357b6b
Hwee-Boon Yar <hboon@motionobj.com>
2024-09-05 13:23:30 +0800

    feat: 🎸 style Quick Donate and Donate to Charity

--- tokenscript/src/routes/DonateToCharity.svelte
+++ tokenscript/src/routes/DonateToCharity.svelte
@@ -76,48 +76,60 @@
 	<div class="flex flex-col items-center">
 		{#if state === 'initial'}
 			{#if walletAddress && tokenId}
-				<h3 class="text-lg font-semibold mb-2">Donate to Charity</h3>
-				<p class="text-sm text-gray-600">Choose a Charity and an amount to donate</p>
-				<p class="text-sm text-gray-500">Charity</p>
-				<div>
-					<select id="charities" bind:value={charityID} class="rounded-md border-2 border-gray-100 p-2">
+				<h3 class="text-xl font-semibold mt-14">Donate to Charity</h3>
+				<p class="text text-gray-600 mt-3">Choose a Charity and an amount to donate</p>
+				<p class="text-sm text-gray-500 mt-6 w-full ml-6">Charity</p>
+				<div class="w-full mt-1 px-3">
+					<select id="charities" bind:value={charityID} class="rounded-lg border-2 border-gray-100 w-full bg-white h-14">
 						<option value="" disabled>Select Charity</option>
 						{#each charities as charity}
 							<option value={charity}>{charity}</option>
 						{/each}
 					</select>
 				</div>
-				<p class="text-sm text-gray-500">Amount</p>
-				<input type="text" bind:value={amount} placeholder="0.00" class="rounded-md border-2 border-gray-100 p-2"/>
-				<p class="text-sm text-gray-600">Suggested amounts</p>
-				<div>
+				<p class="text-sm text-gray-500 mt-8 w-full ml-6">Amount</p>
+				<div class="w-full mt-1 px-3">
+					<div class="flex items-center rounded-lg border-2 border-gray-100">
+						<input type="text" bind:value={amount} placeholder="0.00" class="w-full bg-white h-14 px-2 py-4 text-3xl"/>
+						<svg width="36" height="36" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M12.9479 0.5L12.7842 1.04676V16.9125L12.9479 17.0731L20.4457 12.7199L12.9479 0.5Z" fill="#343434"/>
+							<path d="M12.948 0.5L5.4502 12.7199L12.948 17.0732V9.3724V0.5Z" fill="#8C8C8C"/>
+							<path d="M12.9478 18.4678L12.8555 18.5783V24.23L12.9478 24.4948L20.45 14.1167L12.9478 18.4678Z" fill="#3C3C3B"/>
+							<path d="M12.948 24.4943V18.4673L5.4502 14.1162L12.948 24.4943Z" fill="#8C8C8C"/>
+							<path d="M12.9482 17.0732L20.4459 12.72L12.9482 9.37256V17.0732Z" fill="#141414"/>
+							<path d="M5.4502 12.72L12.9479 17.0732V9.37256L5.4502 12.72Z" fill="#393939"/>
+						</svg>
+						<span class="text-xl pl-1.5 pr-4">ETH</span>
+					</div>
+				</div>
+				<div class="flex justify-between w-full px-3 mt-8">
+					<!--hhh3 Get ETH balance-->
+					<span class="text-sm text-gray-600">Suggested amounts</span><span class="text-sm text-indigo-500"><span>Balance: </span><span class="underline">0.002 ETH</span></span>
+				</div>
+				<div class="flex justify-evenly w-full mt-6">
 					<button
 						type="button"
-						class="text-purple-300 bg-purple-50 border-2 border-purple-300 rounded-2xl px-2 py-1"
+						class="text-indigo-300 border-2 border-indigo-300 rounded-3xl px-5 py-2"
 						on:click={() => setWithDefaultAmount(0.001)}>0.001</button
 					>
 					<button
 						type="button"
-						class="text-purple-300 bg-purple-50 border-2 border-purple-300 rounded-2xl px-2 py-1"
+						class="text-indigo-300 border-2 border-indigo-300 rounded-3xl px-5 py-2"
 						on:click={() => setWithDefaultAmount(0.002)}>0.002</button
 					>
 					<button
 						type="button"
-						class="text-purple-300 bg-purple-50 border-2 border-purple-300 rounded-2xl px-2 py-1"
+						class="text-indigo-300 border-2 border-indigo-300 rounded-3xl px-5 py-2"
 						on:click={() => setWithDefaultAmount(0.003)}>0.003</button
 					>
 					<button
 						type="button"
-						class="text-purple-300 bg-purple-50 border-2 border-purple-300 rounded-2xl px-2 py-1"
+						class="text-indigo-300 border-2 border-indigo-300 rounded-3xl px-5 py-2"
 						on:click={() => setWithDefaultAmount(0.004)}>0.004</button
 					>
 				</div>
-				<div class="w-full p-2">
-					<button
-						type="button"
-						on:click={() => donate()}
-						class="w-full py-4 bg-blue-700 text-white rounded hover:bg-blue-800">Confirm</button
-					>
+				<div class="w-full p-3 mt-5">
+					<button type="button" on:click={() => donate()} class=" w-full py-4 bg-indigo-500 hover:bg-indigo-700 text-white rounded-xl text-xl">Review</button>
 				</div>
 			{/if}
 		{:else if state === 'pending sign or txn confirmation'}
--- tokenscript/src/routes/QuickDonate.svelte
+++ tokenscript/src/routes/QuickDonate.svelte
@@ -49,19 +49,20 @@
 	<div class="flex flex-col items-center">
 	{#if state === 'initial'}
 		{#if walletAddress && tokenId}
-			<h3 class="text-lg font-semibold mb-2">Quick donate</h3>
-			<p class="text-sm text-gray-600">You are donating</p>
-			<p class="text-xl font-semibold">
-0.001 ETH
-			</p>
-			<p class="text-sm text-gray-600">
-				to
-			</p>
-			<p class="text-lg font-semibold text-purple-300 bg-purple-50 border-2 border-purple-300 rounded-3xl p-2">
-				Random Charity
-			</p>
-			<div class="w-full p-2">
-				<button type="button" on:click={() => donate()} class="w-full py-4 bg-blue-700 text-white rounded hover:bg-blue-800">Confirm</button>
+			<h3 class="text-xl font-semibold mt-14">Quick donate</h3>
+			<p class="text text-gray-600 mt-3">You are donating</p>
+			<p class="text-2xl mt-4">0.001 ETH</p>
+			<p class="text text-gray-600 mt-4">to</p>
+			<div class="flex items-center text text-purple-400 bg-purple-100 border-2 border-purple-200 rounded-3xl px-6 py-1 mt-4">
+				<svg width="20" height="20" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M15.3437 11.2812L17.125 13.0625L15.3437 14.8438M15.3437 4.15625L17.125 5.9375L15.3437 7.71875M2.875 13.0625H6.03635C6.52505 13.0625 7.00619 12.9418 7.43707 12.7113C7.86795 12.4807 8.23524 12.1473 8.50635 11.7407L10 9.5" stroke="#DC79FF" stroke-width="1.18812" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M2.875 5.9375H6.03635C6.52505 5.93751 7.00619 6.05816 7.43707 6.28875C7.86795 6.51934 8.23524 6.85273 8.50635 7.25934L11.4937 11.7407C11.7648 12.1473 12.132 12.4807 12.5629 12.7113C12.9938 12.9418 13.475 13.0625 13.9637 13.0625H15.9375M15.9375 5.9375H13.9637C13.475 5.93751 12.9938 6.05816 12.5629 6.28875C12.132 6.51934 11.7648 6.85273 11.4937 7.25934L11.1875 7.71875" stroke="#DC79FF" stroke-width="1.18812" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+
+				<p class="ml-2">Random Charity</p>
+			</div>
+			<div class="w-full p-2 mt-5">
+				<button type="button" on:click={() => donate()} class=" w-full py-4 bg-indigo-500 hover:bg-indigo-700 text-white rounded-x text-xl">Review</button>
 			</div>
 		{/if}
 	{:else if state === 'pending sign or txn confirmation'}
