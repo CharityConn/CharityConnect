c6b7f5e96ea901f00311ede378ba8dab917dec24
Hwee-Boon Yar <hboon@motionobj.com>
2024-09-11 13:15:13 +0800

    feat: 🎸 make Vote show coming soon instead of linking to Snapshot

--- tokenscript/src/routes/Vote.svelte
+++ tokenscript/src/routes/Vote.svelte
@@ -1,44 +1,35 @@
+<script context="module" lang="ts">
+	import type { IWeb3LegacySDK } from '@tokenscript/card-sdk/dist/types';
+
+	declare let tokenscript: IWeb3LegacySDK;
+</script>
+
 <script lang="ts">
 	import context from '../lib/context';
 	import Loader from '../components/Loader.svelte';
 
-	let token;
-	let walletAddress;
 	let loading = true;
-	let points = -1;
 
 	context.data.subscribe(async (value) => {
 		if (!value.token) return;
-		token = value.token;
-		walletAddress = value.token.ownerAddress;
-		points = value.token.points;
 
 		// You can load other data before hiding the loader
 		loading = false;
 	});
-
-	function openVote() {
-		window.open('https://testnet.snapshot.org/#/charityconnect.eth', '_blank');
-	}
 </script>
 
 <div>
-	<div class="flex flex-col items-center w-full px-6">
-		{#if token}
-			<h3 class="text-xl font-semibold mt-14 text-ccBlack">Vote for Charity Fund Allocation</h3>
-			<p class="text text-ccGray mt-3 text-center">Vote on how the charity funds should be allocated. Voting weight is based on your points (ERC-20 token holdings)</p>
-
-			{#if points >= 0}
-				{#if points >= 1}
-					<p class="text text-ccGray mt-3 text-center">You currently have {points / Math.pow(10, 18)} points.</p>
-					<div class="w-full mt-5">
-						<button type="button" on:click={openVote} class="w-full py-4 bg-ccPurple-dark hover:bg-indigo-700 text-white rounded-xl text-xl">Vote here</button>
-					</div>
-				{:else}
-					<p class="text text-ccGray mt-3 text-center">You don't have points yet. Donate to get some.</p>
-				{/if}
-			{/if}
-		{/if}
+	<div class="flex flex-col items-center w-full px-6 mt-14">
+		<svg width="57" height="56" viewBox="0 0 57 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect x="0.5" width="56" height="56" rx="28" fill="#C4C4FF" fill-opacity="0.5"/>
+			<path d="M28.5 16C21.875 16 16.5 21.375 16.5 28C16.5 34.625 21.875 40 28.5 40C35.125 40 40.5 34.625 40.5 28C40.5 21.375 35.125 16 28.5 16Z" stroke="#6868FF" stroke-width="2" stroke-miterlimit="10"/>
+			<path d="M28.5 20V29H34.5" stroke="#6868FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+		<h3 class="text-xl font-semibold mt-6 text-ccBlack">Coming soon!</h3>
+		<p class="text text-ccGray mt-3 text-center">Voting will be available shortly!</p>
+		<div class="w-full mt-5">
+			<button type="button" on:click={tokenscript.action.closeCard} class="w-full py-4 bg-ccPurple-dark hover:bg-indigo-700 text-white rounded-xl text-xl">Ok</button>
+		</div>
 	</div>
 	<Loader show={loading} />
 </div>
