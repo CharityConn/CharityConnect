a7d43ca052d2aec0370c60f11b75d33980bb8fbc
Hwee-Boon Yar <hboon@motionobj.com>
2024-09-11 10:44:33 +0800

    feat: 🎸 add Trade for CHARITYeet

--- tokenscript/src/App.svelte
+++ tokenscript/src/App.svelte
@@ -1,3 +1,9 @@
+<script context="module" lang="ts">
+	import type { IWeb3LegacySDK } from '@tokenscript/card-sdk/dist/types';
+
+	declare let tokenscript: IWeb3LegacySDK;
+</script>
+
 <script lang="ts">
 	import './tailwind.css';
 	import context from './lib/context';
@@ -11,6 +17,7 @@
 	let token;
 	let initialised = false;
 
+	//#trade is not in this map, but hardcoded in `routeChange()`
 	const routingMap = {
 		'#donateToCharity': DonateToCharity,
 		'#quickDonate': QuickDonate,
@@ -22,6 +29,11 @@
 	let page;
 
 	function routeChange() {
+		if (document.location.hash == '#trade') {
+			window.open('https://app.uniswap.org/swap?chain=arbitrum&exactField=output&outputCurrency=0x3a65C4cBd896c3681C471EBa8dA44828c8912043&inputCurrency=0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', '_blank');
+			tokenscript.action.closeCard()
+			return
+		}
 		page = routingMap[token.level == 0 ? '#adopt' : document.location.hash] || NotFound;
 	}
 
